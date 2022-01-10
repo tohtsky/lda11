70b4089079dbe5eb58930a5e2c4013913f4affa2
Tomoki <tomoki.ohtsuki129@gmail.com>
2022-01-10 12:17:40 +0900

    test utils

--- lda11/__init__.py
+++ lda11/__init__.py
@@ -1,4 +1,5 @@
 from .labelled_lda import LabelledLDA
 from .lda import LDA, MultilingualLDA
+from .util import rowwise_train_test_split
 
-__all__ = ["LDA", "LabelledLDA", "MultilingualLDA"]
+__all__ = ["LDA", "LabelledLDA", "MultilingualLDA", "rowwise_train_test_split"]
